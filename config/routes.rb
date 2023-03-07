6722ea50e05cffffe1ae11d205c82fcd92ac74fe
MarioCesarBais <mariocfbais@gmail.com>
2023-03-07 20:20:03 -0300

    React on Rails working: CRUD recipes"

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+# Rails.application.routes.draw do
+#   namespace :api do
+#     namespace :v1 do
+#       get 'recipes/index'
+#       get 'recipes/create'
+#       get 'recipes/show'
+#       get 'recipes/destroy'
+#     end
+#   end
+#   root 'homepage#index'
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+
+
+
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
+  root 'homepage#index'
+  get '/*path' => 'homepage#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
